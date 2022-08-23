b0f70f59a1401390a4cd1f193a1a0fc7c245806e
marcioalv10 <marcioelx2@gmail.com>
2022-08-23 00:20:56 -0300

    Função preencher os algorismos

--- app/src/main/java/br/com/cotemig/calcinteligente/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/calcinteligente/MainActivity.kt
@@ -25,12 +25,50 @@ class MainActivity : AppCompatActivity() {
         binding.bt1.setOnClickListener {
             preenche("1")
         }
+        binding.bt2.setOnClickListener {
+            preenche("2")
+        }
+        binding.bt3.setOnClickListener {
+            preenche("3")
+        }
+        binding.bt4.setOnClickListener {
+            preenche("4")
+        }
+        binding.bt5.setOnClickListener {
+            preenche("5")
+        }
+        binding.bt6.setOnClickListener {
+            preenche("6")
+        }
+        binding.bt7.setOnClickListener {
+            preenche("7")
+        }
+        binding.bt8.setOnClickListener {
+            preenche("8")
+        }
+        binding.bt9.setOnClickListener {
+            preenche("9")
+        }
+
+
+        binding.btAC.setOnClickListener {
+            binding.tvVisor.text = ""
+            valorStr = ""
+        }
+
 
 
     }
 
     private fun preenche(s: String) {
-        valorStr = s
+
+        if(!valorStr.equals("0")){
+
+            valorStr += s
+        }else{
+            valorStr = s
+        }
+
         binding.tvVisor.text = valorStr
     }
 }
