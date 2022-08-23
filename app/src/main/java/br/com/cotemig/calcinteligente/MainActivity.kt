2194fbef7ec0f8aa8e4d56f86d6dfdc74c60ee5a
marcioalv10 <marcioelx2@gmail.com>
2022-08-23 00:10:05 -0300

    Configurando o viewBinding
    Configurando o viewBinding na tela MainActivity

--- app/src/main/java/br/com/cotemig/calcinteligente/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/calcinteligente/MainActivity.kt
@@ -1,20 +1,36 @@
 package br.com.cotemig.calcinteligente
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
+import androidx.appcompat.app.AppCompatActivity
 import br.com.cotemig.calcinteligente.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // private var _binding: ActivityMainBinding? = null
+    //  private val binding get() = _binding!!
     private lateinit var binding: ActivityMainBinding
+    var valorStr = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
+        binding.bt0.setOnClickListener {
+            preenche("0")
+        }
+        binding.bt1.setOnClickListener {
+            preenche("1")
+        }
+
+
+    }
+
+    private fun preenche(s: String) {
+        valorStr = s
+        binding.tvVisor.text = valorStr
     }
 }
