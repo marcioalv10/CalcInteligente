6a06a9494b18caf03648ab034eb7c270b8488c27
marcioalv10 <marcioelx2@gmail.com>
2022-08-22 23:41:55 -0300

    Adicionando viewBinding

--- app/src/main/java/br/com/cotemig/calcinteligente/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/calcinteligente/MainActivity.kt
@@ -1,11 +1,20 @@
 package br.com.cotemig.calcinteligente
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import br.com.cotemig.calcinteligente.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
